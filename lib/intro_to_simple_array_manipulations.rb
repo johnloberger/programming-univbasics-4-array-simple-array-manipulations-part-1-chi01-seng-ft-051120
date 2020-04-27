eceb3fb3d9d27ce380ba7a8a1f1fb7242577544f
John Loberger <johnloberger@gmail.com>
2020-04-27 18:43:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,14 @@
 def using_push(array, string)
   array.push("violet")
 end
+
+def using_unshift(array, string)
+  array.unshift("test")
+end
+
+def using_pop(array, string)
+  array.pop
+end
+
+def pop_with_args(array, arg)
+  
